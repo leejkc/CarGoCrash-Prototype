02727a746c3bb0f78848a8f93a789fc02e132ded
lee <xlegendman@gmail.com>
2025-02-22 00:19:55 -0500

    final commit?
    presentation is tmr! i hope it goes well :)

--- app/src/main/java/com/example/cargocrash/MainActivity.kt
+++ app/src/main/java/com/example/cargocrash/MainActivity.kt
@@ -184,6 +184,23 @@ fun HomeScreen(
             }
 
         }
+
+        Column(
+            // use lazyColumn if there are more elements that don't fit on screen
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.character),
+                contentDescription = "Character",
+                modifier = Modifier
+                    .width(200.dp)
+                    .clip(shape = RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+        }
     }
 }
 
@@ -197,6 +214,9 @@ fun Video2Screen(onBackClick: () -> Unit) {
     ) {
 
         val scrollState = rememberScrollState()
+        val superHeaderSize = 25.sp
+        val headerSize = 20.sp
+        val textSize = 13.sp
         Column(
             // use lazyColumn if there are more elements that don't fit on screen
             modifier = Modifier
@@ -210,7 +230,7 @@ fun Video2Screen(onBackClick: () -> Unit) {
                 contentDescription = "Title",
                 modifier = Modifier
                     .padding(vertical = 16.dp)
-                    .height(70.dp)
+                    .height(80.dp)
                     .clip(shape = RoundedCornerShape(16.dp)),
                 contentScale = ContentScale.Crop
             )
@@ -222,11 +242,163 @@ fun Video2Screen(onBackClick: () -> Unit) {
                     .padding(horizontal = 16.dp, vertical = 8.dp)
                     .fillMaxWidth()
             ) {
-                Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tellus tellus, accumsan non nisl quis, efficitur accumsan purus. Vivamus iaculis ligula sed maximus lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc non sagittis urna, ac porttitor tortor. Duis dui sapien, vulputate nec enim ut, dignissim rhoncus lorem. Aliquam in vehicula mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent et ligula in leo gravida lobortis ac eu orci. Fusce nulla augue, placerat sit amet velit at, faucibus scelerisque mi. Praesent ante ex, fermentum et pretium ac, porta non massa. Cras placerat mollis fermentum.",
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF5B0C1C)
-                )
+                Column {
+                    Text(
+                        "VIDEO BELOW!",
+                        fontSize = superHeaderSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "The most important thing to keep in mind during this entire process is that you must remain at the scene, at least until police arrive. There could be legal repercussions if you leave. Turn hazard lights on (if necessary) to warn passerby drivers, and plan to stay there for a little bit.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "1. Check for injuries.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "Assess everyone in your own vehicle for wellness. If someone is seriously injured, do not move them. Call 911 if anyone is injured, no matter how minor it may appear. Small injuries can become huge issues if left untreated. Do not take any risks.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "2. Move your vehicle to a safe location.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "If possible, move your vehicle out of the way of traffic if it isn’t already. Move to the shoulder of the road or towards the nearest curb. Get out of your car and go to the side furthest from the road or stay in your car if it isn’t safe to exit. If there’s broken glass, don’t touch it.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "3. Call the police.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "A police report can be critical for your insurance claims (especially if there are injuries or significant damage - over \$2000), and it’s also just helpful, as the police will document the accident and be able to affirm that you were not at fault. Use their non-emergency line if you did not need to previously call 911 (aka. This isn’t an emergency.)\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "3.a.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "However, if the damage is not severe, you don’t necessarily have to call the police, and can instead report the accident to the nearest CRC (Collision Report Centre). The CRC is a Police and Private facility that assists motorists in reporting motor vehicle collisions to the Police.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "3.b.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "If you do choose to go this route, take or have your vehicle taken to the CRC most convenient to you. You should attend with your vehicle. Without exception, vehicles that have to be towed must go directly to the CRC from the collision location.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "3.c.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "Bring your documentation with you to the CRC; Driver's licence, ownership, insurance, details of the accident and other involved parties information. Remember that by law, you must contact the CRC within twenty-four hours of the accident.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "4. Exchange information with the other party.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "Get their contact information (phone number, email, home address), full name, and insurance details (insurance company name, insurance company number). You should also note their license plate number and their drivers license information (home address, drivers license number). If there are witnesses, get their contact information (full name, phone number) as well, for future use.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "5. Document the scene.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "Take photos and videos of the damage done to all vehicles involved, the accident scene, and any relevant road conditions. Don’t let a single thing go unphotographed - this can be valuable for any future insurance claims. Record the time, location, and speed of your vehicle (approximately if not already recorded).\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "6. See a medical professional immediately",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "after being rear-ended will ensure all injuries will be documented and treated. Some injuries, like whiplash, might not be immediately apparent. If you feel “off” (aches, pains) at any point after the incident, be sure to seek medical treatment then.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "7. These next bits are optional, if you have insurance.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "Report the accident to your insurance. Provide them with the documentation you gathered at the accident site and the police report number. Also provide them with any relevant medical reports.\n",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+
+                    Text(
+                        "8. Consult a lawyer if needed.",
+                        fontSize = headerSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C)
+                    )
+                    Text(
+                        "If you face challenges with your insurance claim or believe you need additional compensation for injuries, consider seeking legal advice. A lawyer can help ensure you receive fair treatment from insurance companies.",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF5B0C1C),
+                        fontSize = textSize
+                    )
+                }
             }
             Spacer(modifier = Modifier.height(8.dp))
 
@@ -276,6 +448,7 @@ fun CollisionsPage(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            Spacer(modifier = Modifier.height(64.dp))
             Image(
                 painter = painterResource(id = R.drawable.crashkit),
                 contentDescription = "Title",
